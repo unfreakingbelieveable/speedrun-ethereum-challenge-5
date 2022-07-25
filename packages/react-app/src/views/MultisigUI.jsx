a11c188d33a375ef751899abd748e8a9387b4a7d
unfrigginbelievable <unfrigginbelievable117@gmail.com>
2022-07-25 17:07:33 -0400

    Removed more boilerplate UI

--- packages/react-app/src/views/MultisigUI.jsx
+++ packages/react-app/src/views/MultisigUI.jsx
@@ -18,6 +18,7 @@ export default function MultisigUI({
   writeContracts,
 }) {
   const [newSigner, addNewSigner] = useState("loading...");
+  // let proposals = writeContracts.Test_Multisig.test_getProposals(); 
 
   return (
     <div>
@@ -26,6 +27,7 @@ export default function MultisigUI({
       */}
       <div style={{ border: "1px solid #cccccc", padding: 16, width: 400, margin: "auto", marginTop: 64 }}>
         <h2>Multisig UI:</h2>
+        <Divider />
         <h4>Current Members:</h4>
         <List
         bordered
@@ -41,6 +43,7 @@ export default function MultisigUI({
       < Divider />
       <div style={{ margin: 8}}>
           <AddressInput
+            placeholder="Please Enter an Address"
             onChange={e => {
               addNewSigner(e);
             }}
@@ -82,60 +85,6 @@ export default function MultisigUI({
         mainnetProvider={mainnetProvider}
         startBlock={1}
       />
-
-      <div style={{ width: 600, margin: "auto", marginTop: 32, paddingBottom: 256 }}>
-        <Card>
-          Check out all the{" "}
-          <a
-            href="https://github.com/austintgriffith/scaffold-eth/tree/master/packages/react-app/src/components"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            📦 components
-          </a>
-        </Card>
-
-        <Card style={{ marginTop: 32 }}>
-          <div>
-            There are tons of generic components included from{" "}
-            <a href="https://ant.design/components/overview/" target="_blank" rel="noopener noreferrer">
-              🐜 ant.design
-            </a>{" "}
-            too!
-          </div>
-
-          <div style={{ marginTop: 8 }}>
-            <Button type="primary">Buttons</Button>
-          </div>
-
-          <div style={{ marginTop: 8 }}>
-            <SyncOutlined spin /> Icons
-          </div>
-
-          <div style={{ marginTop: 8 }}>
-            Date Pickers?
-            <div style={{ marginTop: 2 }}>
-              <DatePicker onChange={() => {}} />
-            </div>
-          </div>
-
-          <div style={{ marginTop: 32 }}>
-            <Slider range defaultValue={[20, 50]} onChange={() => {}} />
-          </div>
-
-          <div style={{ marginTop: 32 }}>
-            <Switch defaultChecked onChange={() => {}} />
-          </div>
-
-          <div style={{ marginTop: 32 }}>
-            <Progress percent={50} status="active" />
-          </div>
-
-          <div style={{ marginTop: 32 }}>
-            <Spin />
-          </div>
-        </Card>
-      </div>
     </div>
   );
 }
